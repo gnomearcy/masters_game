bb422b76df7c4ee1676b45494a6ee0f27e18cdea
Tomislav <tmartinc@etfos.hr>
2015-01-30 07:45:17 +0100

    added texture files to github

---
+++ web/textures_test/packages/three/src/lights/point_light.dart
@@ -0,0 +1,28 @@
+part of three;
+
+/**
+ * @author mr.doob / http://mrdoob.com/
+ *
+ * Ported to Dart from JS by:
+ * @author rob silverton / http://www.unwrong.com/
+ */
+
+/// Affects objects using MeshLambertMaterial or MeshPhongMaterial.
+class PointLight extends Light
+{
+  Vector3 _position;
+  /// Light's intensity.
+  num intensity;
+  /// If non-zero, light will attenuate linearly from maximum intensity at light position down to zero at distance.
+  num distance;
+
+  /// Creates a light at a specific position in the scene.
+  ///
+  /// The light shines in all directions (roughly similar to a light bulb.)
+  PointLight( num hex, {this.intensity: 1, this.distance: 0} ) : super( hex )
+  {
+    //THREE.Light.call( this, hex );
+
+    _position = new Vector3.zero();
+  }
+}
